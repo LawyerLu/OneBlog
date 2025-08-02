c50c42336181dcd7b372d283c6286276ad2eed58
羽忆 <love@cily.cc>
2025-08-02 17:55:42 +0800

    Update admin.js
    主题支持CDN静态资源自定义地址

--- static/js/admin.js
+++ static/js/admin.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const tabs = [
         { id: 'tab1', label: '主题说明', selector: null }, // 第一个 Tab 是静态内容
         { id: 'base', label: '基础设置', selector: '[id*="slogan"],[id*="logo"],[id*="logowhite"],[id*="MenuSet"],[id*="Favicon"],[id*="switch"],[id*="Banner"],[id*="Menu"],[id*="Tagbg"],[id*="Webthumb"],[id*="Webtime"],[id*="ICP"],[id*="WA"]'},
-        { id: 'pro', label: '高级设置', selector: '[id*="dnsPrefetch"],[id*="imgSmall"],[id*="BeCode"],[id*="RandomIMG"]' },
+        { id: 'pro', label: '高级设置', selector: '[id*="resource_cdn"],[id*="dnsPrefetch"],[id*="imgSmall"],[id*="BeCode"],[id*="RandomIMG"]' },
         { id: 'social', label: '社交按钮', selector: '[id*="QQ"],[id*="Weixin"],[id*="Email"],[id*="Github"]' },
         { id: 'DIY', label: '样式定制', selector: '[id*="CSS"],[id*="JS"],[id*="themeColor"]' },
     ];
